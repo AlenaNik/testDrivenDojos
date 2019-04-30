f7929493911ad34597306c3259c9db511c2faca0
AlenaNik <alena.nik.digital@gmail.com>
2019-04-30 13:41:59 +0200

    solution

---
+++ characterEncoder.js
@@ -0,0 +1,13 @@
+module.exports = function characterEncoder(word){
+        var string = "";
+        var lowerCaseWord = word.toLowerCase();
+        for (var i = 0; i < lowerCaseWord.length; i++) {
+            if (lowerCaseWord.indexOf(lowerCaseWord[i]) === lowerCaseWord.lastIndexOf(lowerCaseWord[i])) {
+                string += "(";
+            } else {
+                string += ")";
+            }
+        }
+        return string;
+    }
+};
--- characterEncoder.test.js
+++ characterEncoder.test.js
@@ -3,24 +3,22 @@ const assert = require('assert');
 const characterEncoder = require('./characterEncoder.js');
 
 describe ('Testing character encoder.', () => {
-
+    it('should return: true',() => {
+        assert.equal(true, true)
+    });
     it('should return "(((" when encoding "din"',() => {
-        assert.equals(characterEncoder("din"),"(((");
+        assert.equal(characterEncoder("din"),"(((");
     });
 
     it('should return "()()()" when encoding "recede"',() => {
-        assert.equals(characterEncoder("recede"),"()()()");
+        assert.equal(characterEncoder("recede"),"()()()");
     });
 
-    it('should return "()()()" when encoding "recede"',() => {
-        assert.equals(characterEncoder("recede"),"()()()");
+    it('should return ")())())","should ignore case" when encoding "Success"',() => {
+        assert.equal(characterEncoder("Success"),")())())","should ignore case");
     });
 
-    it('should return "()()()" when encoding "recede"',() => {
-        assert.equals(characterEncoderEncode("Success"),")())())","should ignore case");
-    });
-
-    it('should return "()()()" when encoding "recede"',() => {
-        assert.equals(characterEncode("(( @"),"))((");
+    it('should return "(( @"),"))((" when encoding "recede"',() => {
+        assert.equal(characterEncoder("(( @"),"))((");
     });
 });
